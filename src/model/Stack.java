59a2b82fbf5163d16dd5906588e8e14c74f3be8a
Pythagoras <Pythagoras@users.noreply.github.com>
2020-11-07 15:09:27 +0100

    Update Stack.java

--- src/model/Stack.java
+++ src/model/Stack.java
@@ -1,5 +1,61 @@
 package model;
 
 //TODO 01: Umsetzung der Klasse Stack gemäß der Dokumentations des Landes.
-public class Stack {
+public class Stack<ContentType>{
+    private class Node{
+        private Node back,front;
+        private ContentType content;
+
+        public Node(ContentType pContent){
+            content = pContent;
+            back = null;
+            front = null;
+        }
+
+        public ContentType getContent(){
+            return content;
+        }
+
+        public void setBack(Node newBack){
+            this.back = newBack;
+        }
+
+        public Node getFront(){
+            return front;
+        }
+    }
+
+    private Node topEnd;
+
+    public Stack(){
+        topEnd = null;
+    }
+
+    public boolean isEmpty(){
+        if(topEnd == null){
+            return true;
+        }
+        return false;
+    }
+
+    public void push(ContentType pContent){
+        if(pContent != null){
+            Node newNode = new Node(pContent);
+            //Falls die Schlange leer ist...
+            if(this.isEmpty()){
+                topEnd = newNode;
+            }else{ //sonst...
+                topEnd.setBack(newNode);
+                topEnd = newNode;
+            }
+        }
+    }
+    public void pop(){
+        if(!isEmpty()){
+            topEnd = topEnd.getFront();
+        }
+    }
+    public ContentType top(){
+        return topEnd.getContent();
+    }
 }
